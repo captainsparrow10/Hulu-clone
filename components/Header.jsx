8963d2b91eaef4c43ccdf9686100a7272e4e680c
captainsparrow10 <javier1009rm@gmail.com>
2022-08-09 09:32:33 -0500

    bg in the hulu page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,22 @@
 import React from "react";
+import { HomeIcon } from "@heroicons/react/solid";
+import Image from "next/image";
 export default function Header() {
-	return <div>Header</div>;
+	return (
+		<header className="flex justify-between p-4">
+			<div>
+				<HomeIcon className="h-6" />
+			</div>
+			<div>
+				<Image
+					className="bg-transparent"
+					src={require("./hulu-logo-black-and-white.png")}
+					alt="Hulu"
+					width={200}
+					height={100}
+					layout="fixed"
+				/>
+			</div>
+		</header>
+	);
 }
